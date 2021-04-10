deafc6348710c323c76b8000c0e6b74324882126
Schaffer <mr.giancarloschaffer@gmail.com>
2021-04-10 02:22:44 -0300

    Create spin the bottle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./bottle.png";
+import _ from "lodash";
+import "./App.scss";
+import { Modal, Input, Button } from "antd";
 
 function App() {
+  const [rotate, setRotate] = useState(false);
+  const [people, setPeople] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleClick = () => {
+    if (!rotate) {
+      setRotate(true);
+      setSelected([]);
+      setTimeout(() => {
+        setRotate(false);
+        setSelected(_.sampleSize(people, 2));
+      }, 1000);
+    }
+  };
+
+  const handleOk = () => {
+    let str = input.replace(/\s+/gim, "").trim();
+    if (str.includes(";")) {
+      str = str.replace(/\W+/gim, ",");
+    }
+    setPeople(str.split(","));
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {people.length === 0 && (
+        <Modal
+          title="Add peoples name separated by , . ;  or /"
+          visible={true}
+          footer={[
+            <Button
+              type="primary"
+              onClick={handleOk}
+              disabled={input.length <= 0}
+            >
+              Ok
+            </Button>,
+          ]}
         >
-          Learn React
-        </a>
-      </header>
+          <Input value={input} onChange={handleChange} />
+        </Modal>
+      )}
+      <div className="group">
+        {selected.map((p) => (
+          <span className="people">{p}</span>
+        ))}
+      </div>
+      <img
+        src={logo}
+        className={`bottle${rotate ? " rotate" : ""}`}
+        alt="bottle"
+        onClick={handleClick}
+      />
     </div>
   );
 }
